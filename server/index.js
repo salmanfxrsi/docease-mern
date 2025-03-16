a29d44b110977eb385eb88c37304be1f3b796ea5
salmanfxrsi <salmanxprivate@gmail.com>
2025-03-16 21:20:04 +0600

    feat (server): deployed server on Vercel

--- server/index.js
+++ server/index.js
@@ -7,8 +7,8 @@ const app = express();
 const port = process.env.PORT || 9000;
 
 // Middleware
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 // MongoDB Connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.upkox.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
